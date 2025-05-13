3d12368d903c086f85a82ca7548e57a3dca6358f
ferrentino01 <justiniato2001@gmail.com>
2025-05-13 17:29:14 +0200

    Update card.py

--- card.py
+++ card.py
@@ -13,13 +13,3 @@ class Card:
 
     def __str__(self):
         return f"{self.value}{self.suit}"
-
-    def get_filename(self):
-        # Mappa seme → lettera file
-        suit_map = {'♠': 'S', '♥': 'H', '♦': 'D', '♣': 'C'}
-        value_map = {'10': '10', 'J': 'J', 'Q': 'Q', 'K': 'K', 'A': 'A',
-                     '2': '2', '3': '3', '4': '4', '5': '5', '6': '6',
-                     '7': '7', '8': '8', '9': '9'}
-        suit_letter = suit_map[self.suit]
-        value_letter = value_map[self.value]
-        return f"images/{value_letter}{suit_letter}.png"
