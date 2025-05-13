20399b533954dc57f4ede6dedb59631959c036f2
ferrentino01 <justiniato2001@gmail.com>
2025-05-13 17:27:12 +0200

    Update card.py

--- card.py
+++ card.py
@@ -7,9 +7,19 @@ class Card:
         if self.value in ['J', 'Q', 'K']:
             return 10
         elif self.value == 'A':
-            return 11  # il valore potrà essere adattato a 1 durante il gioco
+            return 11
         else:
             return int(self.value)
 
     def __str__(self):
         return f"{self.value}{self.suit}"
+
+    def get_filename(self):
+        # Mappa seme → lettera file
+        suit_map = {'♠': 'S', '♥': 'H', '♦': 'D', '♣': 'C'}
+        value_map = {'10': '10', 'J': 'J', 'Q': 'Q', 'K': 'K', 'A': 'A',
+                     '2': '2', '3': '3', '4': '4', '5': '5', '6': '6',
+                     '7': '7', '8': '8', '9': '9'}
+        suit_letter = suit_map[self.suit]
+        value_letter = value_map[self.value]
+        return f"images/{value_letter}{suit_letter}.png"
