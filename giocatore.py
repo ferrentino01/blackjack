d4d832aa2c81f14d1ba2db66402e8d7ab1dfc873
ferrentino01 <justiniato2001@gmail.com>
2025-05-22 17:36:46 +0200

    Add files via upload

---
+++ giocatore.py
@@ -0,0 +1,33 @@
+
+from carta import Carta
+
+class Giocatore:
+    def __init__(self):
+        self.saldo = 1000
+        self.sconfitte = 0
+        self.mano = []
+
+    def punta(self, importo):
+        if 1 <= importo <= self.saldo:
+            self.saldo -= importo
+            return True
+        return False
+
+    def ricevi_carta(self, carta):
+        self.mano.append(carta)
+
+    def calcola_punteggio(self):
+        totale = 0
+        assi = 0
+        for carta in self.mano:
+            valore = carta.get_valore_numerico()
+            totale += valore
+            if carta.valore == 'A':
+                assi += 1
+        while totale > 21 and assi:
+            totale -= 10
+            assi -= 1
+        return totale
+
+    def aggiorna_sconfitte(self):
+        self.sconfitte += 1
