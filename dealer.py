563ddb72301b856d765c14e9701d167a4b6ed10d
ferrentino01 <justiniato2001@gmail.com>
2025-06-06 15:47:38 +0200

    Update dealer.py

--- dealer.py
+++ dealer.py
@@ -1,22 +1,5 @@
+from partecipante import Partecipante
 
-from carta import Carta
-
-class Dealer:
+class Dealer(Partecipante):
     def __init__(self):
-        self.mano = []
-
-    def ricevi_carta(self, carta):
-        self.mano.append(carta)
-
-    def calcola_punteggio(self):
-        totale = 0
-        assi = 0
-        for carta in self.mano:
-            valore = carta.get_valore_numerico()
-            totale += valore
-            if carta.valore == 'A':
-                assi += 1
-        while totale > 21 and assi:
-            totale -= 10
-            assi -= 1
-        return totale
+        super().__init__()
