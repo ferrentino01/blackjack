4850db8e925097180a310d536ff6fe6fd237f9bc
ferrentino01 <justiniato2001@gmail.com>
2025-06-06 17:30:22 +0200

    Update carta.py

--- carta.py
+++ carta.py
@@ -1,5 +1,4 @@
 
-
 class Carta:
     def __init__(self, seme, valore):
         self.seme = seme
@@ -9,6 +8,11 @@ class Carta:
         if self.valore in ['J', 'Q', 'K']:
             return 10
         elif self.valore == 'A':
-            return 11  # sarà gestito a livello di punteggio per evitare superamento del 21
-        else:
-            return int(self.valore)
+            return 11
+        return int(self.valore)
+
+    def __str__(self):
+        return f"{self.valore} di {self.seme}"
+
+    def __repr__(self):
+        return self.__str__()
