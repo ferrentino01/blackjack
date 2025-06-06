49f37c166eab3fff6582234408141cab90b65c1a
ferrentino01 <justiniato2001@gmail.com>
2025-06-06 17:31:45 +0200

    Update partecipante.py

--- partecipante.py
+++ partecipante.py
@@ -1,22 +1,24 @@
-class Partecipante:
+from abc import ABC
+
+class Partecipante(ABC):
     def __init__(self):
         self.mano = []
 
     def ricevi_carta(self, carta):
         self.mano.append(carta)
 
+    def reset_mano(self):
+        self.mano = []
+
     def calcola_punteggio(self):
-        punteggio = 0
+        totale = 0
         assi = 0
         for carta in self.mano:
             valore = carta.get_valore_numerico()
+            totale += valore
             if carta.valore == 'A':
                 assi += 1
-            punteggio += valore
-        while punteggio > 21 and assi:
-            punteggio -= 10
+        while totale > 21 and assi:
+            totale -= 10
             assi -= 1
-        return punteggio
-
-    def reset_mano(self):
-        self.mano = []
+        return totale
