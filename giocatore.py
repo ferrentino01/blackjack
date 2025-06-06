d75a73cab2544c1408f88d8b13a7f5a63f4aaafa
ferrentino01 <justiniato2001@gmail.com>
2025-06-06 17:27:23 +0200

    Update giocatore.py

--- giocatore.py
+++ giocatore.py
@@ -1,9 +1,27 @@
+
 from partecipante import Partecipante
 
 class Giocatore(Partecipante):
     def __init__(self):
         super().__init__()
         self.saldo = 1000
+        self.puntata = 0
+        self.vittorie = 0
+        self.sconfitte = 0
+        self.pareggi = 0
 
-    def aggiorna_saldo(self, importo):
-        self.saldo += importo
+    def punta(self, importo):
+        if 0 < importo <= self.saldo:
+            self.puntata = importo
+            self.saldo -= importo
+            return True
+        return False
+
+    def aggiorna_vittorie(self):
+        self.vittorie += 1
+
+    def aggiorna_sconfitte(self):
+        self.sconfitte += 1
+
+    def aggiorna_pareggi(self):
+        self.pareggi += 1
