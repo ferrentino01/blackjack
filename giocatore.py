a21da92135da733e0e224e88ffae7ab691d09e69
ferrentino01 <justiniato2001@gmail.com>
2025-06-06 15:46:35 +0200

    Update giocatore.py

--- giocatore.py
+++ giocatore.py
@@ -1,32 +1,9 @@
+from partecipante import Partecipante
 
-from carta import Carta
-
-class Giocatore:
+class Giocatore(Partecipante):
     def __init__(self):
+        super().__init__()
         self.saldo = 1000
-        
-        self.mano = []
 
-    def punta(self, importo):
-        if 1 <= importo <= self.saldo:
-            self.saldo -= importo
-            return True
-        return False
-
-    def ricevi_carta(self, carta):
-        self.mano.append(carta)
-
-    def calcola_punteggio(self):
-        totale = 0
-        assi = 0
-        for carta in self.mano:
-            valore = carta.get_valore_numerico()
-            totale += valore
-            if carta.valore == 'A':
-                assi += 1
-        while totale > 21 and assi:
-            totale -= 10
-            assi -= 1
-        return totale
-
-   
+    def aggiorna_saldo(self, importo):
+        self.saldo += importo
